8669017b1b5127966085d6502ab6d1f78fe5a3f6
Cuong Dang <cuong.developer19@gmail.com>
2021-05-27 22:21:53 +0700

    Form Inputs [SHOP APP]

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -61,7 +61,30 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct() {
+  void addProduct(Product product) {
+    var newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
+    notifyListeners();
+  }
+
+  void updateProduct(String id, Product updatedProduct) {
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    if (prodIndex >= 0) {
+      _items[prodIndex] = updatedProduct;
+    } else {
+      // ...
+    }
+    notifyListeners();
+  }
+
+  void deleteProduct(String id) {
+    _items.removeWhere((prod) => prod.id == id);
     notifyListeners();
   }
 
---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './edit_product_screen.dart';
+import '../providers/products.dart';
+import '../widgets/app_drawer.dart';
+import '../widgets/user_product_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              UserProductItem(
+                id: productsData.items[i].id,
+                title: productsData.items[i].title,
+                imageUrl: productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
