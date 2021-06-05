b473a2633a944455fa51c85b1f718155694ee7b7
Cuong Dang <cuong.developer19@gmail.com>
2021-06-05 21:32:01 +0700

    Building Animations [SHOP APP]

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './providers/cart.dart';
 import './providers/products.dart';
 import './providers/orders.dart';
 import './providers/auth.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -51,6 +52,10 @@ class MyApp extends StatelessWidget {
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              TargetPlatform.android: CustomPageTransitionBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionBuilder(),
+            }),
           ),
           home: authData.isAuth
               ? ProductsOverviewScreen()
